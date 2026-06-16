322191fef382682605407dfa2524e64d8ef4a4d7
leechan7942 <leechan9606@gmail.com>
2026-06-16 06:26:00 +0000

    path error edit

--- drone_practice/launch/setup_px4_env.sh
+++ drone_practice/launch/setup_px4_env.sh
@@ -2,9 +2,9 @@
 # PX4 경로를 실제 설치 위치로 바꾸세요
 export PX4_DIR=$HOME/PX4-Autopilot
 
-source $PX4_DIR/Tools/setup_gazebo.bash $PX4_DIR $PX4_DIR/build/px4_sitl_default
+source $PX4_DIR/Tools/simulation/gazebo-classic/setup_gazebo.bash $PX4_DIR $PX4_DIR/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$PX4_DIR
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$PX4_DIR/Tools/sitl_gazebo
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$PX4_DIR/Tools/simulation/gazebo-classic/sitl_gazebo-classic
 
 # 우리 모델 경로 추가
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$(rospack find drone_practice)/models
